f3c1211147b34bfb498ae045d3243942600cf026
Waleed Munir <iwaleedmunir006@gmail.com>
2026-04-17 22:04:15 +0500

    Added Mobile App Project

---
+++ DodhiApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DodhiApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
